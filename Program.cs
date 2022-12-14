99105770e90e07f0beb50e75278e9b4dd546054d
OVGI <girichevao@gmail.com>
2022-12-14 13:29:02 +0300

    add task41

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3        */
+
+string[] numbers = Console.ReadLine()!.Split(' ');
+int[] array = new int[numbers.Length];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    int.TryParse(numbers[i], out array[i]);
+    Console.Write($"{array[i]} ");
+}
+int k = 0;
+foreach (var i in array)
+    if (i > 0)
+
+        k++;
+Console.Write($" -> {k} чисел больше 0");
